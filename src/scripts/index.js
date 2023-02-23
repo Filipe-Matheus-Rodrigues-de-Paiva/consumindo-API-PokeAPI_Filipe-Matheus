f810764b96799f651ee918c0e09068c4790b063b
filipe-matheus <filipepaivinha@gmail.com>
2023-02-23 11:58:05 -0300

    Primeira versão do PokeAPI

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,59 @@
-/* Monte sua lógica aqui */
+import { consomePokeAPI, getPokemonByName } from "./requests.js";
+
+
+setTimeout( () => {
+    async function renderizaPokemons() {
+        const ulTag = document.querySelector('.pokemon__list')
+    
+        ulTag.innerHTML = '';
+    
+        // Aqui utilizaremos o await para aguardar a resposta da função
+        const listaDePokemons = await consomePokeAPI()
+    
+        // Agora é só iterar sob o array e renderizar cada pokemon
+        listaDePokemons.results.forEach(pokemon => {
+            // "Fatia" o número do pokemon na pokedex a partir da URL de cada pokemon
+            const numeroNaPokedex = pokemon.url.slice(34, -1)
+    
+            ulTag.insertAdjacentHTML('beforeend', `
+                <li class="pokemon__card">
+                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${numeroNaPokedex}.png" alt=${pokemon.name}>
+                    <span>${pokemon.name}</span>
+                </li>
+            `)
+        })
+    }
+    renderizaPokemons()
+}, 800)
+
+
+function searchPokemon () {
+    const input = document.querySelector('input');
+
+    input.addEventListener('keyup', async (e) => {
+        
+        e.preventDefault();
+
+        const pokemonEncontrado = await getPokemonByName(input.value.trim());
+
+        const listaDePokemons = await consomePokeAPI()
+        
+        const ulTag = document.querySelector('.pokemon__list')
+        listaDePokemons.results.forEach(pokemon => {
+            if (pokemonEncontrado == pokemon.name) {
+                ulTag.innerHTML = '';
+
+                const numeroNaPokedex = pokemon.url.slice(34, -1)
+
+                ulTag.insertAdjacentHTML('beforeend', `
+                <li class="pokemon__card">
+                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${numeroNaPokedex}.png" alt=${pokemon.name}
+                    <span>${pokemon.name}</span>
+                </li>
+            `)
+            }
+        })
+    })
+}
+
+searchPokemon()
--- src/scripts/requests.js
+++ src/scripts/requests.js
@@ -1,1 +1,37 @@
-/* Monte a lógica das requisições aqui */
+export async function consomePokeAPI() {
+    // Seleciona o elemento que representa o loading da requisição
+    const loading = document.querySelector('.loading')
+    const form = document.querySelector('.hidden');
+
+    // Faz a requisição na API
+    const pokemonsDaAPI = await fetch('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151')
+      .then(
+        /*  Converte o retorno para um objeto Javascript válido */
+        response => response.json()
+      )
+      .catch(
+        /* Caso haja algum erro, retornamos ele no console */
+        error => console.log(error)
+      )
+    
+    // Independente da requisição ser um sucesso, ou um erro, removeremos o loading da tela e adicionaremos a barra de pesquisa
+    setTimeout( () => {
+      loading.classList.remove('loading');
+      loading.classList.add('hidden');
+      form.classList.remove('hidden');
+      form.classList.add('show');
+    }, 100)
+
+    // Retorna esse valor convertido
+    return pokemonsDaAPI
+}
+
+export async function getPokemonByName (pokemonName) {
+    const pokemonFound = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+    .then(response => response.json())
+    .then(res => {
+        return res.name;
+    })
+
+    return pokemonFound;
+}
